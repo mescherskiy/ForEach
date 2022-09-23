c425ad7759b0d6ab35ad207944f799c1f3f8a396
anatoliy <omelchenko20@gmail.com>
2022-09-23 15:26:21 +0300

    Made class CustomUser impl. UserDetails

--- ForEach/src/main/java/ua/com/foreach/model/CustomUser.java
+++ ForEach/src/main/java/ua/com/foreach/model/CustomUser.java
@@ -1,13 +1,20 @@
 package ua.com.foreach.model;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.util.Collection;
+import java.util.Collections;
 
 @Data
+@NoArgsConstructor
 @Entity
 @Table(name = "users")
-public class CustomUser {
+public class CustomUser implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -23,6 +30,57 @@ public class CustomUser {
     @Column(name = "role")
     private Role role;
     @Enumerated(value = EnumType.STRING)
-    @Column(name = "status")
-    private Status status;
+/*    @Column(name = "status")
+    private Status status;*/
+    private Boolean locked = false;
+    private Boolean enabled = false;
+
+    public CustomUser(String firstName,
+                   String lastName,
+                   String email,
+                   String password,
+                   Role role) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        SimpleGrantedAuthority authority =
+                new SimpleGrantedAuthority(role.name());
+        return Collections.singletonList(authority);
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !locked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
 }
