2e29bd15c90825383f7baba88513ebe5854119ec
anatoliy <omelchenko20@gmail.com>
2022-09-23 15:30:55 +0300

    Added rule for registration end-point to SecurityConfig

--- ForEach/src/main/java/ua/com/foreach/config/SecurityConfig.java
+++ ForEach/src/main/java/ua/com/foreach/config/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/").permitAll()
+                .antMatchers("/", "/registration/**").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
