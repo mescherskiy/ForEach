5d9820109d10ccc663b861c61f04c76498d045bf
anatoliy <omelchenko20@gmail.com>
2022-09-23 16:14:47 +0300

    Added EmailService for send registration_confirm-link

---
+++ ForEach/src/main/java/ua/com/foreach/email/EmailSender.java
@@ -0,0 +1,5 @@
+package ua.com.foreach.email;
+
+public interface EmailSender {
+    void send(String to, String email);
+}
---
+++ ForEach/src/main/java/ua/com/foreach/email/EmailService.java
@@ -0,0 +1,38 @@
+package ua.com.foreach.email;
+
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+@Service
+@AllArgsConstructor
+public class EmailService implements EmailSender{
+
+    private final static String FAIL_SEND_MSG = "failed to send email";
+    private final static Logger LOGGER = LoggerFactory.getLogger(EmailService.class);
+    private final JavaMailSender mailSender;
+
+    @Override
+    @Async
+    public void send(String to, String email) {
+        try {
+            MimeMessage mimeMessage = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
+            helper.setText(email, true);
+            helper.setTo(to);
+            helper.setSubject("Confirm your email");
+            helper.setFrom("foreach.public@gmail.com");
+            mailSender.send(mimeMessage);
+        } catch (MessagingException e) {
+            LOGGER.error(FAIL_SEND_MSG, e);
+            throw new IllegalArgumentException(FAIL_SEND_MSG);
+        }
+    }
+}
